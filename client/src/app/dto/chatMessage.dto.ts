7d3cf1c403f9b92126671394947fee5211e02a96
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2022-11-25 21:15:25 +0400

    Add & upd dtos

---
+++ client/src/app/dto/chatMessage.dto.ts
@@ -0,0 +1,11 @@
+import { Chat } from "./chat.dto";
+import { User } from "./user.dto";
+
+export interface ChatMessage {
+    id?: string;
+    content: string;
+    sender: User;
+    chat: Chat;
+    created: Date;
+    updated: Date;
+}
