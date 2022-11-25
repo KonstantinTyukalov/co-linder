7b94e05a920378e91593a906c95b748de08981af
Evgenii Manin💜 <jovannym@mail.ru>
2022-11-25 17:24:00 +0400

    Client in progress IV

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegistrationComponent } from "../components/registration/registration.component";
-import { LoginComponent } from "../components/login/login.component";
+import { RegistrationComponent } from "./components/registration/registration.component";
+import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,8 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from "../components/login/login.component";
-import { RegistrationComponent } from "../components/registration/registration.component";
+import { StoreModule } from '@ngrx/store';
+import { RegistrationComponent } from "./components/registration/registration.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @NgModule({
     declarations: [
@@ -14,7 +15,8 @@ import { RegistrationComponent } from "../components/registration/registration.c
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        StoreModule.forRoot({})
     ],
     providers: [],
     bootstrap: [AppComponent]
