33ffb19f0b00a230a2c0fda28a72cd14a6bef73b
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2022-11-26 01:55:48 +0400

    chat component

---
+++ client/src/app/store/selectors/chat.selectors.ts
@@ -0,0 +1,8 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { ChatsState } from "../reducers/chat.reducer";
+
+export const state = createFeatureSelector<ChatsState>('CHAT_STATE');
+
+export const chats = createSelector(state, state => state.chats);
+
+export const chat = createSelector(state, state => state.currentChat);
