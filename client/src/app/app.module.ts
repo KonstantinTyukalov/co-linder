b3362cc96116d0953d99bbc6c9100b152be21b3e
Evgenii Manin💜 <v-evmanin@microsoft.com>
2022-11-25 14:52:42 +0400

    Client in progress I

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from '../components/login/login.component';
+import { RegistrationComponent } from "../components/registration/registration.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+        RegistrationComponent
     ],
     imports: [
         BrowserModule,
---
+++ client/src/components/registration/registration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-registration',
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent {
+    onClick() {
+        //this.store.dispatch(userActions.registration);
+    }
+}
