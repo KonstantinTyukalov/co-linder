8c5288a026ed9db0c89fdb3a4771d2c91a140c9f
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2022-11-25 23:58:44 +0400

    flat component

--- client/src/app/components/flat/flat.component.ts
+++ client/src/app/components/flat/flat.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from "@ngrx/store";
+
+import * as FlatActions from '../../store/actions/flat.actions'
+import { Flat } from "../../dto/flat.dto";
+
+import * as FlatSelector from '../../store/selectors/flat.selectors';
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'app-flat',
@@ -6,13 +13,13 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./flat.component.scss']
 })
 export class FlatComponent implements OnInit {
-    ///private flat$ = this.store.select()
+    public flat$: Observable<Flat> = this.store.select(FlatSelector.flat);
 
-    constructor() {
+    constructor(private readonly store: Store) {
     }
 
     ngOnInit(): void {
-        ///this.store.dispatch();
+        this.store.dispatch(FlatActions.getFlatById({ id: '' }));
     }
 
 }
--- client/src/app/store/effects/flat.effects.ts
+++ client/src/app/store/effects/flat.effects.ts
@@ -4,6 +4,7 @@ import { FlatService } from '../../services/flat.service';
 import * as FlatActions from '../actions/flat.actions';
 import { from, switchMap } from "rxjs";
 import { map } from "rxjs/operators";
+import { Flat } from "src/app/dto/flat.dto";
 
 @Injectable()
 export class FlatEffects {
@@ -19,8 +20,20 @@ export class FlatEffects {
             switchMap(() => {
                 return from(this.flatService.getFlats());
             }),
-            map((flats) => {
-                return FlatActions.getFlatsSuccess({ flats: flats });
+            map((flats: Flat[]) => {
+                return FlatActions.getFlatsSuccess({ flats });
+            })
+        )
+    })
+
+    getFlatById$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(FlatActions.getFlatById),
+            switchMap((action: { id: string }) => {
+                return from(this.flatService.getFlatById(action.id))
+            }),
+            map((flat: Flat) => {
+                return FlatActions.getFlatByIdSuccess({ flat });
             })
         )
     })
