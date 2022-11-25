6e27a6010badf6a2fa6980cbe757decc149b4758
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2022-11-26 00:19:31 +0400

    Fix user dto

--- client/src/app/dto/user.dto.ts
+++ client/src/app/dto/user.dto.ts
@@ -1,10 +1,10 @@
 export interface User {
     id?: string;
     email: string;
-    avatar: string;
+    avatar?: string;
     name: string;
-    age: number;
-    isWoman: boolean | null;
+    age?: number;
+    isWoman?: boolean | null;
     country?: string;
     languages?: string;
     password?: string;
