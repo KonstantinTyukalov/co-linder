83e0d89fb56565eaef93e9001d626233adcd2b88
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2022-11-25 21:13:43 +0400

    flat

--- client/src/app/store/reducers/flat.reducer.ts
+++ client/src/app/store/reducers/flat.reducer.ts
@@ -1,3 +1,33 @@
+import { Flat } from '../../dto/flat.dto';
+import { createReducer, on } from '@ngrx/store';
+import * as FlatActions from '../actions/flat.actions';
+
 export interface FlatState {
-    
+    flat: Flat;
+    flatNotFoundErr: string;
 }
+
+export const initialState: FlatState = {
+    flat: {
+        id?: string;
+        externalUrl?: string;
+        photo?: File;
+        name: string; // "Flat in center" or address
+        owner: User;
+        area: string; // city or region in city
+        cost: number;
+        capacity: number;
+        description: string;
+        created: Date;
+        updated: Date;
+    },
+    productNotFoundErr: '',
+};
+
+export const reducer = createReducer(
+    initialState,
+    on(FlatActions.getFlatsSuccess, (state, { product }) => ({
+        ...state,
+        product: product,
+    })),
+);
