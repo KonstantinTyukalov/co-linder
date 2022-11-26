bf9b277a1586ef8e20dd770d80e65dfdcb5f5c63
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2022-11-26 14:31:55 +0400

    refactoring main.go

--- server-go/main.go
+++ server-go/main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"log"
-
-	"github.com/pocketbase/pocketbase"
+    "log"
+    "github.com/pocketbase/pocketbase"
 )
 
 func main() {
-	app := pocketbase.New()
+    app := pocketbase.New()
 
-	// app.OnCollectionsListRequest().Add(func(e *core.CollectionsListEvent) error {
-	// 	log.Println(e.Result)
-	// 	return nil
-	// })
-
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
-	}
+    if err := app.Start(); err != nil {
+        log.Fatal(err)
+    }
 }
