2f9764c45fba8a3febf15765c2478c91c9fcc276
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2022-11-28 02:55:21 +0400

    Upd Flat pb model

--- client/src/app/models/flat.model.pb.ts
+++ client/src/app/models/flat.model.pb.ts
@@ -3,13 +3,13 @@ import { FlatCommentPb } from './flatComment.model.pb';
 import { UserPb } from './user.model.pb';
 
 export interface FlatPb extends BaseModelPb {
-    externalUrl: string;
-    name: string;
+    externalUrl?: string;
+    name?: string;
     owner: string;
     area: string;
     cost: number;
     description: string;
-    capacity: number;
+    capacity?: number;
     interestedUsers: string[];
     readyToLiveUsers: string[];
     comments: string[];
