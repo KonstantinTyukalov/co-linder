cb083fe1ba61b7c8d7371dc163188fe42e12be94
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2022-12-04 17:27:50 +0400

    Upd start & build server scripts

--- server-go/scripts/start.ps1
+++ server-go/scripts/start.ps1
@@ -1,1 +1,22 @@
-go run main.go serve --http="172.16.101.93:3000"
+param(
+    [switch]$Docker
+)
+
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+$serverProjectSource = Join-Path $scriptPath ..
+
+$localIpAddr = (Get-NetIPAddress -InterfaceAlias Wi-Fi -AddressFamily IPv4).IPAddress
+
+$port = 3000
+
+if ($Docker) {
+    docker run -d --name coliving-app-server -t -p ${port}:${port} -v coliving-app-data://usr/local/bin/app/pb_data qinamrug/coliving-crew-server:latest
+}
+else {
+    $entryPointPath = Join-Path $serverProjectSource cmd main.go
+    go run $entryPointPath serve --http="${localIpAddr}:${port}"
+}
+
+
+# sudo docker container create qinamrug/coliving-crew-server
