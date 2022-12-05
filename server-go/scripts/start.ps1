92435234536567fb2263c8a97a216617755749c8
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2022-12-06 03:20:21 +0400

    Fix start script

--- server-go/scripts/start.ps1
+++ server-go/scripts/start.ps1
@@ -1,10 +1,10 @@
 param(
     [switch]$Docker
 )
-
+$startPath = Get-Location
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
-$serverProjectSource = Join-Path $scriptPath ..
+$serverProjectSource = Join-Path $scriptPath .. | Resolve-Path
 
 $localIpAddr = (Get-NetIPAddress -InterfaceAlias Wi-Fi -AddressFamily IPv4).IPAddress
 
@@ -14,9 +14,9 @@ if ($Docker) {
     docker run -d --name coliving-app-server -t -p ${port}:${port} -v coliving-app-data://usr/local/bin/app/pb_data qinamrug/coliving-crew-server:latest
 }
 else {
-    $entryPointPath = Join-Path $serverProjectSource cmd main.go
-    go run $entryPointPath serve --http="${localIpAddr}:${port}"
+    $entryPointPath = Join-Path $serverProjectSource cmd
+    Set-Location $entryPointPath
+    go run main.go serve --http="${localIpAddr}:${port}"
 }
-
 
 # sudo docker container create qinamrug/coliving-crew-server
